dbfbc2cdd11cf6782264ca6bbf07095f5463c30c
Del0611 <muhfadelramadan06@gmail.com>
2026-02-17 15:55:08 +0700

    up

--- UKK3/routes/web.php
+++ UKK3/routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
-
-//database query test
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+    // Ambil semua data dari tabel admin
+    $dataAdmin = DB::table('admin')->get();
 
-Route::get('/cek-data', function () {
-    // Mengambil semua data dari tabel 'admin'
-    $data = DB::table('admin')->get();
-
-    // Menampilkan data ke layar dalam format yang mudah dibaca
-    return dd($data);
+    // Tampilkan data ke layar browser
+    return response()->json($dataAdmin);
 });
-
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
